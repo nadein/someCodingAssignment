d34bc3b0b40b329c0490a5cb0b27087f07fcce16
Alex Nadein <alexander@rxphoto.com>
2021-02-26 14:19:03 +0200

    Added API key for TMDb. Added development team for successful building/signing.

--- Moviestar/Application/Session.swift
+++ Moviestar/Application/Session.swift
@@ -12,6 +12,6 @@ final class Session {
     let client: TMDbClient
 
     init() {
-        client = TMDbClient(apiKey: "<INSERT-API-KEY-HERE>")
+        client = TMDbClient(apiKey: "2ff20a4ef65eda1c0f884a031a2d319f")
     }
 }
