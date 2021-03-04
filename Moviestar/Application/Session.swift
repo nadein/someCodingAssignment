c0c94cf2ce5acd189cd3f304d4ba7510d1d22bdc
Alex Nadein <alexander@rxphoto.com>
2021-03-04 18:58:45 +0200

    Added comment about git-crypt.

--- Moviestar/Application/Session.swift
+++ Moviestar/Application/Session.swift
@@ -13,5 +13,6 @@ final class Session {
 
     init() {
         client = TMDbClient(apiKey: "2ff20a4ef65eda1c0f884a031a2d319f")
+        // It's unsafe to store any keys in the source code like this. So in a production project, I would use git-crypt to automatically encrypt files containing secrets.
     }
 }
